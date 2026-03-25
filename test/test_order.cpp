be57a7b120cf4ab21f29d2dbf28a0a01c6ea4ec8
Pixiedust3 <ccsirus@gmail.com>
2026-03-25 11:26:12 +0545

    GoodTillCancel

--- test/test_order.cpp
+++ test/test_order.cpp
@@ -1,3 +1,21 @@
 #include <catch2/catch_all.hpp>
 #include "Order.h"
 
+SCENARIO("Orders can be filled ")
+{
+    GIVEN("GoodTillCancel")
+    {
+        OrderId id = 123;
+        Price price = 100;
+        Quantity qty = 50;
+        Order order{ OrderType::GoodTillCancel, id, Side::Buy, price, qty };
+
+        THEN("The initial state is correctly set") {
+            REQUIRE(order.GetOrderId() == id);
+            REQUIRE(order.GetPrice() == price);
+            REQUIRE(order.GetRemainingQuantity() == qty);
+            REQUIRE(order.GetFilledQuantity() == 0);
+            REQUIRE_FALSE(order.IsFilled());
+        }
+    }
+}
